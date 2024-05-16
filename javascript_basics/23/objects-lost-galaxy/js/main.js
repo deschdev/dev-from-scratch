e9fb30b6ed3f000e73158a95493858361494c5ae
deschdev <94028614+deschdev@users.noreply.github.com>
2024-05-15 21:17:54 -0400

    completed review and started API

--- javascript_basics/23/objects-lost-galaxy/js/main.js
+++ javascript_basics/23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,21 @@
 //Create a dog object that has four properties and three methods
+const dog = {
+    breed: "catahoula",
+    name: "renly",
+    age: 9,
+    behavior: "lax",
+    bark() {
+        console.log("bark bark bark")
+    },
+    walk() {
+        console.log("walking")
+    },
+    eat() {
+        console.log("eat")
+    },
+    sniff() {
+        console.log("sniff sniff sniff")
+    }
+}
+
+console.log(dog.behavior)
--- javascript_basics/23/space-review/js/main.js
+++ javascript_basics/23/space-review/js/main.js
@@ -1,12 +1,29 @@
 //Arrays
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
-
+let numbersOne = [1, 2, 3, 4, 5];
+const sum = numbersOne.reduce((a, b) => a + b);
+// alert(sum); 
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
+let numbersTwo = [6, 7, 8, 9, 10];
+function squared(arr) {
+   const sq = arr.map((num) => num ** 2);
+   return sq;
+}
 
+console.log(squared(numbersTwo));
 //Create a function that takes string
 //Print the reverse of that string to the console
-
+let soonToBeReversedString = "polkadot";
+function reverse(str) {
+    const split = str.split("");
+    const reverse = split.reverse();
+    const joinStr = reverse.join("");
+    console.log(joinStr);
+}
+reverse(soonToBeReversedString);
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+const palindrome = str => alert(str === str.split("").reverse().join(""));
+palindrome("racecar")
